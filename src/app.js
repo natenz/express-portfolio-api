80e1d7e4f43e8d72a6d581c526a303e8db2298c1
Natz <adrianputradinata@gmail.com>
2026-01-08 23:47:05 +0700

    Corsts

--- src/app.js
+++ src/app.js
@@ -1,44 +1,34 @@
 const express = require("express");
-const errorHandler = require("./middleware/error.middleware");
 const cors = require("cors");
+const errorHandler = require("./middleware/error.middleware");
 const app = express();
 
+// CORS global
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "OPTIONS"], // tambahin OPTIONS
+    origin: "http://localhost:3000", // hanya FE dev
+    methods: ["GET", "POST", "OPTIONS"], // tambahin OPTIONS
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-// middleware
+
+// biar preflight OPTIONS jalan
 app.options("*", cors());
+
+// middleware
 app.use(express.json());
 
 // routes
-const profileRoute = require("./routes/profile.route");
-const experienceRoute = require("./routes/experience.route");
-const skillRoute = require("./routes/skill.route");
-const educationRoute = require("./routes/education.route");
-const certificationRoute = require("./routes/certification.route");
-
-// base routes
-app.use("/profile", profileRoute);
-app.use("/experience", experienceRoute);
-app.use("/skills", skillRoute);
-app.use("/education", educationRoute);
-app.use("/certifications", certificationRoute);
+app.use("/profile", require("./routes/profile.route"));
+app.use("/experience", require("./routes/experience.route"));
+app.use("/skills", require("./routes/skill.route"));
+app.use("/education", require("./routes/education.route"));
+app.use("/certifications", require("./routes/certification.route"));
 app.use(errorHandler);
 
 // health check
-console.log("Health check endpoint registered at /");
 app.get("/", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "Portfolio API is running 🚀",
-  });
-});
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
+  res.json({ status: "OK", message: "Portfolio API is running 🚀" });
 });
 
 module.exports = app;
